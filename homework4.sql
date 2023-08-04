64eff31acf7466725cebbf61ab3d9d19700a8060
Ceyhun Onac <ceyhunonac58@gmail.com>
2023-08-04 15:51:22 +0300

    Homework 4 SQL File Added

---
+++ homework4.sql
@@ -0,0 +1,18 @@
+1)
+SELECT DISTINCT replacement_cost FROM film;
+
+2)
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+3)
+SELECT title,rating FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+4)
+SELECT * FROM country
+WHERE country LIKE '_____';
+
+
+5)
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%R';
